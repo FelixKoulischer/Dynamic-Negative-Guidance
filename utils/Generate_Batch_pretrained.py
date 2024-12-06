99db32d6315d7a1b391bb7eb65319d79c37d8800
FelixKoulischer <94680319+FelixKoulischer@users.noreply.github.com>
2024-12-06 13:27:57 +0100

    Update Generate_Batch_pretrained.py

--- utils/Generate_Batch_pretrained.py
+++ utils/Generate_Batch_pretrained.py
@@ -51,21 +51,21 @@ def generate_batch(N_batch: int, diffusion: nn.Module, model: nn.Module, to_forg
             - diffusion       : The forward diffusion processs, containing the noising schedule
             - model           : The unconditional model
             - to_forget_model : The to-forget model, that can only generate forbidden images
-            - guidance_type   : The guidance type, should be one of negative_prompting, safe_latent_diffusion, negative_guidance
+            - guidance_type   : The guidance type, should be one of negative_prompting, safe_latent_diffusion, dynamic_negative_guidance
             - device          : The devic eon which models should run
             - seed            : The fixed seed (Optional)
             - initial_noise   : The initial sampled noise from N(0,1) (Optional)
             - C, H, W         : The dimension of the to generate images (Default: 1,32,32)
             - guidance_scale  : The guidance scale $\lambda$ (Default: 1.)
             - threshold, s_s, beta_m, s_m : The additional hyperparameters for SLD (Required for SLD)
-            - prior, Temp     : The additional hyperparameters for NG (Required for NG)
-            - p_min, p_max    : Clamping of posterior (Optional for NG)
-            - offset          : Additional bias for NG (Optional for NG)
+            - prior, Temp     : The additional hyperparameters for DNG (Required for DNG)
+            - p_min, p_max    : Clamping of posterior (Optional for DNG)
+            - offset          : Additional bias for DNG (Optional for DNG)
     Output:
             - A torch.Tensor of size (N_batch,C,H,W) containing the N_batch generated images
     '''
     set_seed(seed)
-    assert guidance_type in ['safe_latent_diffusion', 'negative_prompting', 'negative_guidance'], "Guidance type is not defined! Should be one of: safe_latent_diffusion, negative_prompting and negative_guidance "
+    assert guidance_type in ['safe_latent_diffusion', 'negative_prompting', 'dynamic_negative_guidance'], "Guidance type is not defined! Should be one of: safe_latent_diffusion, negative_prompting and dynamic_negative_guidance "
 
     if initial_noise is None:
         initial_noise= torch.randn((N_batch, C, H, W)).to(device)
@@ -80,9 +80,9 @@ def generate_batch(N_batch: int, diffusion: nn.Module, model: nn.Module, to_forg
         return gen_batch_SLD(N_batch, diffusion, model, to_forget_model, device, initial_noise, guidance_scale, threshold, s_s, beta_m, s_m)
 
     # Negative guidance
-    if guidance_type == 'negative_guidance':
+    if guidance_type == 'dynamic_negative_guidance':
         assert prior is not None and Temp is not None, "Wrong hyperparameters passed for Negative Guidance. Should be given: (prior, Temp). Optional: (p_min, p_max)" 
-        return gen_batch_NG(N_batch, diffusion, model, to_forget_model, device, initial_noise, guidance_scale, prior, Temp, p_min, p_max,offset)
+        return gen_batch_DNG(N_batch, diffusion, model, to_forget_model, device, initial_noise, guidance_scale, prior, Temp, p_min, p_max,offset)
 
 
 def gen_batch_NP(N_batch: int, diffusion: nn.Module, model: nn.Module, to_forget_model: nn.Module, device: str, initial_noise: torch.Tensor,
@@ -147,7 +147,7 @@ def gen_batch_SLD(N_batch: int, diffusion: nn.Module, model: nn.Module, to_forge
             - beta_m          : The damping factor for the momentum term
             - s_m             : The rescaling of the momentum term
     Output:
-            - A torch.Tensor of size (N_batch,C,H,W) containing the N_batch generated images using Negative prompting
+            - A torch.Tensor of size (N_batch,C,H,W) containing the N_batch generated images using Safe Latent Diffusion
     '''
     batch = initial_noise.to(device)
     SLD_correc_with_mom = torch.zeros(batch.size()).to(batch.device)
@@ -183,10 +183,10 @@ def gen_batch_SLD(N_batch: int, diffusion: nn.Module, model: nn.Module, to_forge
         denoised_x = noised_x  
         return denoised_x
 
-def gen_batch_NG(N_batch: int, diffusion: nn.Module, model: nn.Module, to_forget_model: nn.Module, device: str, initial_noise: torch.Tensor,
+def gen_batch_DNG(N_batch: int, diffusion: nn.Module, model: nn.Module, to_forget_model: nn.Module, device: str, initial_noise: torch.Tensor,
                  guidance_scale: float, prior: float, Temp: float, p_min: float, p_max: float, offset: float) -> torch.Tensor:
     '''
-    Function to generate a batch of images using Safe Latent Diffusion (SLD) as guidance mechanism
+    Function to generate a batch of images using Dynamic Negative Guidance (DNG) as guidance mechanism
     
     Input:
             - N_batch         : The number of desired images
@@ -203,7 +203,7 @@ def gen_batch_NG(N_batch: int, diffusion: nn.Module, model: nn.Module, to_forget
             - p_max           : For clamping of posterior: maximum value (Optional, default = 0.99)
             - offset          : Additional offset for the added term (Optional, default = 0.0)
     Output:
-            - A torch.Tensor of size (N_batch,C,H,W) containing the N_batch generated images using Negative prompting
+            - A torch.Tensor of size (N_batch,C,H,W) containing the N_batch generated images using Dynamic Negative Guidance
     '''
     batch = initial_noise.to(device)
     
@@ -229,14 +229,14 @@ def gen_batch_NG(N_batch: int, diffusion: nn.Module, model: nn.Module, to_forget
                 new_noised_x = new_noised_x + noise_for_SDE
     
             # Compute the posterior
-            p = compute_posterior_NG(diffusion, ti, p, new_noised_x, noised_x, noise_pred, to_forget_noise_pred,
+            p = compute_posterior_DNG(diffusion, ti, p, new_noised_x, noised_x, noise_pred, to_forget_noise_pred,
                                      Temp=Temp, p_min=p_min, p_max=p_max, offset = offset)
     
             # Update the state
             noised_x = new_noised_x
         return noised_x
 
-def compute_posterior_NG(diffusion: nn.Module, t: int, p_prev: torch.Tensor, new_noised_x: torch.Tensor, noised_x: torch.Tensor,
+def compute_posterior_DNG(diffusion: nn.Module, t: int, p_prev: torch.Tensor, new_noised_x: torch.Tensor, noised_x: torch.Tensor,
                          noise_pred: torch.Tensor, to_forget_noise_pred: torch.Tensor, 
                          Temp: float=1.0, p_min: float=1e-6, p_max : float= 0.99, offset: float=0.) -> torch.Tensor:
     '''
@@ -250,15 +250,15 @@ def compute_posterior_NG(diffusion: nn.Module, t: int, p_prev: torch.Tensor, new
             - noised_x              : The noisy state at timestep t+1
             - noise_pred            : The noise prediction of unconditional model at timestep t+1
             - to_forget_noise_pred  : The noise prediction of bad model at timestep t+1
-            - Temp                  : The temperature hyperparameter for NG (Default = 1.)
+            - Temp                  : The temperature hyperparameter for DNG (Default = 1.)
             - p_min                 : For clamping of posterior: minimum value (Default = 1e-6)
             - p_max                 : For clamping of posterior: maximum value (Default = 0.99)
-            - offset          : Additional offset for the added term (Optional, default = 0.0)
+            - offset                : Additional offset for the added term (Optional, default = 0.0)
 
     Output:
             - A torch.Tensor of size (N_batch) containing the estimated posterior value for each of the images being generated
     '''
-    a_t, a_bar_t = diffusion.alphas[t], diffusion.alphas_cumprod[t]          # Diffusion constants required for Negative Guidance
+    a_t, a_bar_t = diffusion.alphas[t], diffusion.alphas_cumprod[t]                      # Diffusion constants required for Dynamic Negative Guidance
     a_bar_tmin1 = diffusion.alphas_cumprod[t-1] if t>0 else a_bar_t
     sigma_square_t = (1-a_bar_tmin1)/(1-a_bar_t)*(1-a_t)
     
